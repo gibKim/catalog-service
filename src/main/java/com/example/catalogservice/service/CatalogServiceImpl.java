941e13a7e9a5b60550a1f55a79fb693135b3b5f0
김기봉 <gibkim@gmarket.com>
2022-06-02 16:13:56 +0900

    Initial commit

---
+++ src/main/java/com/example/catalogservice/service/CatalogServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.catalogservice.service;
+
+import com.example.catalogservice.entity.CatalogEntity;
+import com.example.catalogservice.entity.CatalogRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Data
+@Slf4j
+@Service
+@AllArgsConstructor
+public class CatalogServiceImpl implements CatalogService{
+    CatalogRepository catalogRepository;
+
+    @Override
+    public Iterable<CatalogEntity> getAllCatalogs() {
+        return catalogRepository.findAll();
+    }
+}
